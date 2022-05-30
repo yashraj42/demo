d6a29f0fe3b5465e61e2d132ba6bd9f831e0b74d
yashraj42 <59300202+yashraj42@users.noreply.github.com>
2022-05-30 18:28:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn1 = document.querySelector('#btn-1');
+
+btn1.addEventListener('click', ()=>{
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1400x400/?coding,technology')";
+})
